8416c29b30dcea8979ef9c73a529c20fcf1aa5a8
newgamer11 <qfpotw4u81@naver.com>
2024-10-10 11:17:10 +0900

    default settings

---
+++ BE/cloud_1st_JWT_MFA/src/main/java/dev/cloud/Cloud1stJwtMfaApplication.java
@@ -0,0 +1,13 @@
+package dev.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloud1stJwtMfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cloud1stJwtMfaApplication.class, args);
+	}
+
+}
---
+++ BE/cloud_1st_JWT_MFA/src/test/java/dev/cloud/Cloud1stJwtMfaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cloud1stJwtMfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
