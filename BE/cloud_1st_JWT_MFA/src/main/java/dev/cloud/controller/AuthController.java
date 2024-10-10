6ec7964727c8a159b6e03163aaac363c5633500c
yaejinkong <0910a@naver.com>
2024-10-10 17:14:49 +0900

    auth ê미구현

--- BE/cloud_1st_JWT_MFA/src/main/java/dev/cloud/controller/AuthController.java
+++ BE/cloud_1st_JWT_MFA/src/main/java/dev/cloud/controller/AuthController.java
@@ -1,7 +1,14 @@
 package dev.cloud.controller;
 
+import dev.cloud.dto.TokenDto;
+import dev.cloud.dto.TokenRequestDto;
+import dev.cloud.dto.UserRequestDto;
+import dev.cloud.dto.UserResponseDto;
 import dev.cloud.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserResponseDto> signup(@RequestBody UserRequestDto userRequestDto) {
+        return ResponseEntity.ok(authService.signup(userRequestDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody UserRequestDto userRequestDto) {
+        return ResponseEntity.ok(authService.login(userRequestDto));
+    }
+
+//    @PostMapping("/reissue")
+//    public ResponseEntity<UserResponseDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
+//        return ResponseEntity.ok(authService.reissue(tokenRequestDto));
+//    }
 }
--- BE/cloud_1st_JWT_MFA/src/main/java/dev/cloud/service/AuthService.java
+++ BE/cloud_1st_JWT_MFA/src/main/java/dev/cloud/service/AuthService.java
@@ -1,15 +1,20 @@
 package dev.cloud.service;
 
 import dev.cloud.dto.TokenDto;
+import dev.cloud.dto.TokenRequestDto;
 import dev.cloud.dto.UserRequestDto;
 import dev.cloud.dto.UserResponseDto;
 import dev.cloud.jwt.TokenProvider;
+import dev.cloud.model.RefreshToken;
 import dev.cloud.model.User;
 import dev.cloud.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -29,4 +34,29 @@ public class AuthService {
         User user = userRequestDto.toUser(passwordEncoder);
         return UserResponseDto.of(userRepository.save(user));
     }
+
+    @Transactional
+    public TokenDto login(UserRequestDto userRequestDto) {
+        UsernamePasswordAuthenticationToken authenticationToken = userRequestDto.toAuthentication();
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+
+//        RefreshToken refreshToken = RefreshToken.builder()
+//                .key(authentication.getName())
+//                .value(tokenDto.getRefreshToken())
+//                .build();
+//
+//        refreshTokenRepository.save(refreshToken);
+
+        // 5. 토큰 발급
+        return tokenDto;
+    }
+
+
+//    public UserResponseDto reissue(TokenRequestDto tokenRequestDto) {
+//
+//
+//    }
 }
