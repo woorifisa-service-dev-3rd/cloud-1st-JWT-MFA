8f23ecc901464f4effed98329fe40a13e79e64e5
ChanUi <hcu55@naver.com>
2024-10-10 12:09:56 +0900

    model, resources 세팅

---
+++ BE/cloud_1st_JWT_MFA/src/main/java/dev/cloud/model/TokenBlacklist.java
@@ -0,0 +1,25 @@
+package dev.cloud.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "token_blacklist")
+public class TokenBlacklist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 512)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+}
